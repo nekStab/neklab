7373d0ba278aa26d0675d12c81b4d71cece12415
Jean-Christophe <loiseau.jc@gmail.com>
2024-04-11 07:21:37 +0200

    LightKrylov re-exports.

--- core/neklab.f90
+++ core/neklab.f90
@@ -1,12 +1,42 @@
       module neklab
+      
+      ! --> Abstract Krylov methods.
       use LightKrylov
+      ! --> Definition of the abstract vectors in the Nek framework.
       use neklab_vectors
+      ! --> Definitions of the abstract linops in the Nek framework.
       use neklab_linops
       
       private
       
+      !------------------------------------------
+      !-----     LIGHTKRYLOV RE-EXPORTS     -----
+      !------------------------------------------
+      
+      ! Global variables.
       public :: wp
-      public :: nek_dvector, nopcopy
+      
+      ! Krylov factorizations.
+      public :: initialize_krylov_subspace
+      public :: arnoldi_factorization, krylov_schur_restart
+      public :: lanczos_bidiagonalization, lanczos_tridiagonalization
+      
+      ! Matrix factorizations.
+      public :: eigs, eighs, svds
+      public :: save_eigenspectrum
+      
+      
+      !----------------------------------
+      !-----     NEKLAB EXPORTS     -----
+      !----------------------------------
+      
+      ! Definition of the abstract vectors in the Nek framework.
+      public :: nek_dvector
+      
+      ! Implementation of the standard linear operators.
       public :: exponential_propagator
+
+      ! Various utilities.
+      public :: nopcopy
       
       end module neklab
