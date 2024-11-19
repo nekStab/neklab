4c2dd64052d66aa4be4fe7b3977ccd80cac7acb7
Jean-Christophe <loiseau.jc@gmail.com>
2024-11-19 15:03:11 +0100

    Added rmatvec for resolvent.

--- src/linops/resolvent.f90
+++ src/linops/resolvent.f90
@@ -27,47 +27,80 @@
             select type(vec_out)
             type is (nek_zvector)
                ! Compute forced-response with zero initial condition.
-               b = evaluate_rhs(vec_in, self%omega)
+               b = evaluate_rhs(vec_in, self%omega, ifadj)
                ! Solve the linear system for the real part.
-               vec_out%re = solve_resolvent_real_part(exptA, b)
+               vec_out%re = solve_resolvent_real_part(exptA, b, ifadj)
                ! Evaluate the imaginary part.
                exptA%tau = tau/4.0_dp; call exptA%init()
-               vec_out%im = evaluate_imaginary_part(vec_in, self%omega, vec_out%re)
+               vec_out%im = evaluate_imaginary_part(vec_in, self%omega, vec_out%re, ifadj)
             end select
          end select
          end procedure
       
          module procedure resolvent_rmatvec
+         type(exptA_linop) :: exptA
+         type(nek_dvector) :: b
+         real(kind=dp) :: tau
+         ! Force adjoint mode.
+         ! Integration time.
+         tau = merge(1.0_dp, 2*pi_dp/abs(self%omega), self%omega == 0.0_dp)
+         ! Initialize exponential propagator.
+         exptA = exptA_linop(tau, self%baseflow); call exptA%init()
+         ! Resolvent computation.
+         select type(vec_in)
+         type is (nek_zvector)
+            select type(vec_out)
+            type is (nek_zvector)
+               ! Compute forced-response with zero initial condition.
+               b = evaluate_rhs(vec_in, self%omega, .true.)
+               ! Solve the linear system for the real part.
+               vec_out%re = solve_resolvent_real_part(exptA, b, .true.)
+               ! Evaluate the imaginary part.
+               exptA%tau = tau/4.0_dp; call exptA%init()
+               vec_out%im = evaluate_imaginary_part(vec_in, self%omega, vec_out%re, .true.)
+            end select
+         end select
          end procedure
 
          !-------------------------------------
          !-----     Utility functions     -----
          !-------------------------------------
 
-         function evaluate_rhs(forcing, omega) result(b)
+         function evaluate_rhs(forcing, omega, adjoint) result(b)
             type(nek_zvector), intent(in) :: forcing
             real(dp), intent(in) :: omega
+            logical, intent(in) :: adjoint
             type(nek_dvector) :: b
             complex(dp) :: alpha
+            real(dp) :: sign
+            if (adjoint) then
+               ifadj = .true.; call bcast(ifadj, lsize)
+            endif
             ! Set the forcing function.
             neklab_forcing => neklab_resolvent_forcing
+            ! Set the sign (adjoint or direct).
+            sign = merge(-1.0_dp, 1.0_dp, adjoint)
             ! Time integration.
             call opzero(vxp, vyp, vzp)
             time = 0.0_dp; lastep = 0; fintim = param(10)
             do istep = 1, nsteps
                ! Update the resolvent forcing for the corresponding time.
-               alpha = exp(im_cdp*omega*time)
+               alpha = exp(sign*im_cdp*omega*time)
                call resolvent_forcing%axpby(zero_cdp, forcing, alpha)
                ! Nek5000 step.
                call nek_advance()
             enddo
             call nek2vec(b, vxp, vyp, vzp, prp, tp)
             neklab_forcing => dummy_forcing
+            if (adjoint) then
+               ifadj = .false.; call bcast(ifadj, lsize)
+            endif
          end function
 
-         function solve_resolvent_real_part(exptA, b) result(x)
+         function solve_resolvent_real_part(exptA, b, adjoint) result(x)
             type(nek_dvector), intent(inout) :: b
             type(exptA_linop), intent(in) :: exptA
+            logical, intent(in) :: adjoint
             type(nek_dvector) :: x
             ! Local variables.
             type(gmres_dp_opts) :: opts
@@ -76,34 +109,45 @@
             real(dp), parameter :: tol = 1.0e-6_dp
             real(dp) :: alpha
             ! Initialize S = I - exptA.
-            S = axpby_linop_rdp(Id_rdp(), exptA, 1.0_dp, -1.0_dp); call x%zero()
+            S = axpby_linop_rdp(Id_rdp(), exptA, 1.0_dp, -1.0_dp)
+            call x%zero()
             ! GMRES call.
             call logger_setup(nio=0, log_level=information_level, log_stdout=.true., log_timestamp=.true.)
             opts = gmres_dp_opts(kdim=64)
-            call gmres(S, b, x, info, options=opts, atol=1.0e-12_dp, rtol=tol)
+            call gmres(S, b, x, info, options=opts, atol=1.0e-12_dp, rtol=tol, transpose=adjoint)
          end function
 
-         function evaluate_imaginary_part(forcing, omega, x) result(y)
+         function evaluate_imaginary_part(forcing, omega, x, adjoint) result(y)
             type(nek_zvector), intent(in) :: forcing
             real(dp), intent(in) :: omega
             type(nek_dvector), intent(in) :: x
+            logical, intent(in) :: adjoint
             type(nek_dvector) :: y
             complex(dp) :: alpha
+            real(dp) :: sign
+            if (adjoint) then
+               ifadj = .true.; call bcast(ifadj, lsize)
+            endif
             ! Set the forcing function.
             neklab_forcing => neklab_resolvent_forcing
+            ! Set the sign (adjoint or direct).
+            sign = merge(-1.0_dp, 1.0_dp, adjoint)
             ! Initial condition.
             call vec2nek(vxp, vyp, vzp, prp, tp, x)
             ! Time-integration.
             time = 0.0_dp; lastep = 0; fintim = param(10)
             do istep = 1, nsteps
                ! Update resolvent forcing.
-               alpha = exp(im_cdp*omega*time)
+               alpha = exp(sign*im_cdp*omega*time)
                call resolvent_forcing%axpby(zero_cdp, forcing, alpha)
                ! Nek5000 step.
                call nek_advance()
             enddo
             call nek2vec(y, vxp, vyp, vzp, prp, tp)
             neklab_forcing => neklab_resolvent_forcing
+            if (adjoint) then
+               ifadj = .false.; call bcast(ifadj, lsize)
+            endif
          end function
 
          subroutine neklab_resolvent_forcing(ffx, ffy, ffz, ix, iy, iz, ieg)
